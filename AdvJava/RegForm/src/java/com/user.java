7dbdaabfff1967c07bec26ebe833e272eaaf890a
Ruh16 <42055525+Ruh16@users.noreply.github.com>
2018-09-05 00:20:58 +0530

    Add files via upload

---
+++ AdvJava/RegForm/src/java/com/user.java
@@ -0,0 +1,38 @@
+
+
+
+package com;
+
+
+public class user {
+
+    String nm;
+    String add;
+    int age;
+
+    public String getNm() {
+        return nm;
+    }
+
+    public void setNm(String nm) {
+        this.nm = nm;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
